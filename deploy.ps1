ec07bb6d50ffb9d80ef3beb2ec1c9e85fcef989b
Aigars Jepifanovs <aigars.jepifanovs@hortusdigital.com>
2021-03-03 14:58:40 +0200

    small fix

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,11 @@
 $rg  = Get-Date -Format "ddMMyyyyHHmm_RG"
+$Purpose = "Testing"
+$Owner = "Aigars Jepifanovs"
+$Customer = "Hortus Digital"
 
 New-AzResourceGroup -Name $rg -Location westeurope -Tags @{"Purpose"="$Purpose";"Owner"="$Owner";"Customer"="$Customer"} -Force
 
 New-AzResourceGroupDeployment `
 -ResourceGroupName $rg `
--TemplateUri "https://raw.githubusercontent.com/AIGARSJ/ARM/main/101-WindowsVirtualMachine.json" `
--TemplateParameterFile '.\101'
+-TemplateUri "https://raw.githubusercontent.com/AIGARSJ/ARM/main/101-WindowsVirtualMachine/101-WindowsVirtualMachine-Template.json" `
+-TemplateParameterUri "https://raw.githubusercontent.com/AIGARSJ/ARM/main/101-WindowsVirtualMachine/101-WindowsVirtualMachine-Parameter.json"
